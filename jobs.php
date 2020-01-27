fcbe39d1ba5e486346b52aabb0c4bb641e2048e4
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-01-27 14:55:03 -0300

    Constructores y metodos

--- jobs.php
+++ jobs.php
@@ -1,22 +1,47 @@
 <?php
 
 class Job {
-  public $title;
+  
+  private $title;
   public $description;
   public $visible;
   public $months;
+
+
+  public function __construct($t, $d) {
+    $this->title = $t;
+    $this->description = $d;
+    $this->visible = true;
+  }
+  public function getTitle(){
+    return $this->title;
+  }
+
+  public function setTitle($t = 'N/A'){
+    $this->title = $t;
+  }
+
+  public function getDuration(){
+    $years = floor($this->months/12);
+    $extraMonths = $this->months % 12;
+  
+    if ($years == 0) {
+      return "$extraMonths months";
+    }
+  
+    if ($extraMonths == 0) {
+      return "$years years";
+    }
+  
+    return "$years years and $extraMonths months";
+  }
+
 }
 
-$job1 = new Job();
-$job1->title = 'PHP Developer';
-$job1->description = 'This is an awesome job!!!';
-$job1->visible = true;
+$job1 = new Job('PHP Developer', 'This is an awesome job!!!');
 $job1->months = 16;
 
-$job2 = new Job();
-$job2->title = 'C++ Developer';
-$job2->description = 'This is an awesome job!!!';
-$job2->visible = true;
+$job2 = new Job('C++ Developer', 'This is an awesome job!!!');
 $job2->months = 32;
 
 $jobs = [
@@ -24,20 +49,6 @@ $jobs = [
   $job2
 ];
 
-function getDuration($months){
-  $years = floor($months/12);
-  $extraMonths = $months % 12;
-
-  if ($years == 0) {
-    return "$extraMonths months";
-  }
-
-  if ($extraMonths == 0) {
-    return "$years years";
-  }
-
-  return "$years years and $extraMonths months";
-}
 
 function printJob($job){
   if(!$job->visible){
@@ -45,9 +56,9 @@ function printJob($job){
   }
 
   echo '<li class="work-position">';
-  echo '<h5>' . $job->title . '</h5>';
+  echo '<h5>' . $job->getTitle() . '</h5>';
   echo '<p>' . $job->description . '</p>';
-  echo '<p>' . getDuration($job->months) . '</p>';
+  echo '<p>' . $job->getDuration() . '</p>';
   echo '<strong>Achievements:</strong>';
   echo '<ul>';
   echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
